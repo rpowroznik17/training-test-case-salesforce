01120c758530ef446d86cb0ac5afb386208a0054
Rafal <rafalp443@gmail.com>
2022-10-05 11:02:40 +0200

    Fix navigation to accounts page

--- pages/sales_page.py
+++ pages/sales_page.py
@@ -12,9 +12,5 @@ class SalesPage:
         self.browser = browser
 
     def navigate_to_accounts(self):
-        # WebDriverWait(self.browser, 10).until(EC.visibility_of_all_elements_located(self.ACCOUNTS))
-        # self.browser.execute_script("document.querySelector('a[href='/lightning/o/Account/home'] > span').click()")
-        # accounts = self.browser.find_element(*self.ACCOUNTS)
-        # accounts.click()
-        sleep(7)
-        WebDriverWait(self.browser, 20).until(EC.element_to_be_clickable(self.ACCOUNTS)).click()
+        accounts = self.browser.find_element(*self.ACCOUNTS)
+        accounts.click()
